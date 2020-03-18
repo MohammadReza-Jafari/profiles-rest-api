a1c368b7bcbcda949e5fae2eb27e15a664e34d56
MohmmadReza-Jafari <chashm78@gmail.com>
2020-03-19 02:24:57 +0330

    create user profile and user manager and migrate

--- profiles_rest_api/settings.py
+++ profiles_rest_api/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'profiles_rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'profiles_api_db',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -123,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profile_api.UserProfile'
